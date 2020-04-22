1242f29238ae6c1f15ab88f0ce058164a3ad6d0f
Anselmo <ansjunior@msn.com>
2020-04-22 20:51:55 -0300

    Endpoint to return user posts

--- src/main/java/com/anselmo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/anselmo/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.anselmo.workshopmongo.domain.Post;
 import com.anselmo.workshopmongo.domain.User;
 import com.anselmo.workshopmongo.dto.UserDto;
 import com.anselmo.workshopmongo.services.UserService;
@@ -60,4 +61,9 @@ public class UserResource {
 
 	}
 
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }

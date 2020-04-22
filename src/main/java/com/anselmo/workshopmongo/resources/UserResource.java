20ba3f414b83a8bee884daab6d3b2d2d8acdaa49
Anselmo <ansjunior@msn.com>
2020-04-22 19:04:19 -0300

    Delete by DELETE

--- src/main/java/com/anselmo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/anselmo/workshopmongo/resources/UserResource.java
@@ -27,23 +27,28 @@ public class UserResource {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDto>> findAll() {
 		List<User> list = service.findAll();
-		List<UserDto> listDto=list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
+		List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
-
 	}
-	
-	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDto> findById(@PathVariable String id) {
-		User obj=service.findById(id);
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDto(obj));
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDto objDto) {
-		User obj=service.fromDto(objDto);
-		obj=service.insert(obj);
-		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		User obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }

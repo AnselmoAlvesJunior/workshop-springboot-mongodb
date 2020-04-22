ed02ddc77f3b8867dd757147fb926baa9ecbcf93
Anselmo <ansjunior@msn.com>
2020-04-22 19:16:54 -0300

    Update with PUT

--- src/main/java/com/anselmo/workshopmongo/services/UserService.java
+++ src/main/java/com/anselmo/workshopmongo/services/UserService.java
@@ -13,31 +13,41 @@ import com.anselmo.workshopmongo.services.exception.ObjectNotFoudException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoudException("Objeto não encontrado"));		
+		return obj.orElseThrow(() -> new ObjectNotFoudException("Objeto não encontrado"));
 	}
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
-		repo.deleteById(id); 
+		repo.deleteById(id);
 	}
-	
-	public User fromDto(UserDto objDto) {
-		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
-	}
-	
-	
-}
 
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
+}

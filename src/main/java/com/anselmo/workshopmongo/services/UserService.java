9a07b75be5f4fb1625f9b52956ac8f5237cc1365
Anselmo <ansjunior@msn.com>
2020-04-22 18:52:29 -0300

    input user with POST

--- src/main/java/com/anselmo/workshopmongo/services/UserService.java
+++ src/main/java/com/anselmo/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.anselmo.workshopmongo.domain.User;
+import com.anselmo.workshopmongo.dto.UserDto;
 import com.anselmo.workshopmongo.repository.UserRepository;
 import com.anselmo.workshopmongo.services.exception.ObjectNotFoudException;
 
@@ -22,5 +23,14 @@ public class UserService {
 	
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoudException("Objeto não encontrado"));		}
+		return obj.orElseThrow(() -> new ObjectNotFoudException("Objeto não encontrado"));		
+	}
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDto objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
 }
+
